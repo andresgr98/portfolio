7a28f05a35b54fa02dce5cf2d5a60d4f15210221
andres <gomezand15@gmail.com>
2021-07-27 17:37:47 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <nuxt></nuxt>
+  </div>
+</template>
+
+<script>
+import Navbar from "~/components/navbar.vue";
+export default {
+  data() {
+    return {
+      Navbar,
+    };
+  },
+};
+</script>
